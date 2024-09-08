a6d0a5d4467b7943ea2d8dc844fd9ba9ad44e2c1
abu bakar <abubakar850772@gmail.com>
2024-09-08 15:00:32 +0600

    finished add api response and error handling utils

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import dotenv from 'dotenv/config';
 import connectDB from "./db/index.js";
+import app from './app.js';
 
 
 
 connectDB()
-
- 
+.then(() => {
+    app.on( "Error", (error) => {
+        console.log("Error: ", error);
+        throw error;
+        
+    })
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`app is running on port ${process.env.PORT}`);
+        
+    })
+})
+.catch((error) => {
+    console.log("MongoDB Connection Failed", error);
+    
+})
 
 
 
