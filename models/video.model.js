88212a2a1ed24f382f524fcba9f46374aff26053
abu bakar <abubakar850772@gmail.com>
2024-09-08 22:14:10 +0600

    finished file uploading configuration

--- models/video.model.js
+++ models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
     {
@@ -45,5 +46,7 @@ const videoSchema = new Schema(
         timestamps: true
     }
 )
+
+mongoose.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema)
